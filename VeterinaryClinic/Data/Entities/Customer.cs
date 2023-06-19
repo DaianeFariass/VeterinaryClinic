3a9b4c2fae17f91467374131abd754352d68e24a
Daiane Farias <daiane.farias.contractor@gmail.com>
2023-06-19 12:05:49 +0100

    Customers Controller Data Context Entity Customer Init Project

---
+++ VeterinaryClinic/Data/Entities/Customer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace VeterinaryClinic.Data.Entities
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Image")]
+        public Guid ImageId { get; set; }
+
+        [Required]
+        [MaxLength(50, ErrorMessage = "The field {0} can contain {1} characters lenght.")]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(9, ErrorMessage = "The field {0} can contain {1} characters lenght.")]
+        public string Document { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+
+        [Required]
+        public string Phone { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+    }
+}
---
+++ VeterinaryClinic/Migrations/20230619103958_InitDb.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace VeterinaryClinic.Migrations
+{
+    public partial class InitDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ImageId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Document = table.Column<string>(type: "nvarchar(9)", maxLength: 9, nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Phone = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Customers");
+        }
+    }
+}
