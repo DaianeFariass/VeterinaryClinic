6128123ac91efa7f0c9158afcae8d723d910b66b
Daiane Farias <daiane.farias.contractor@gmail.com>
2023-07-03 17:12:12 +0100

    Login and logout

--- VeterinaryClinic/Helpers/IUserHelper.cs
+++ VeterinaryClinic/Helpers/IUserHelper.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System.Threading.Tasks;
 using VeterinaryClinic.Data.Entities;
+using VeterinaryClinic.Models;
 
 namespace VeterinaryClinic.Helpers
 {
@@ -8,6 +9,8 @@ namespace VeterinaryClinic.Helpers
     {
         Task<User> GetUserByEmailAsync(string email);
         Task<IdentityResult> AddUserAsync(User user, string password);
+        Task<SignInResult> LoginAsync(LoginViewModel model);
+        Task LogoutAsync();
 
     }
 }
--- VeterinaryClinic/Helpers/UserHelper.cs
+++ VeterinaryClinic/Helpers/UserHelper.cs
@@ -1,16 +1,20 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System.Threading.Tasks;
 using VeterinaryClinic.Data.Entities;
+using VeterinaryClinic.Models;
 
 namespace VeterinaryClinic.Helpers
 {
     public class UserHelper : IUserHelper
     {
         private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
 
-        public UserHelper(UserManager<User> userManager)
+        public UserHelper(UserManager<User> userManager,
+            SignInManager<User> signInManager)
         {
            _userManager = userManager;
+           _signInManager = signInManager;
         }
         public async Task<IdentityResult> AddUserAsync(User user, string password)
         {
@@ -20,6 +24,20 @@ namespace VeterinaryClinic.Helpers
         public async Task<User> GetUserByEmailAsync(string email)
         {
             return await _userManager.FindByEmailAsync(email);
+        }
+
+        public async Task<SignInResult> LoginAsync(LoginViewModel model)
+        {
+            return await _signInManager.PasswordSignInAsync(
+               model.Username,
+               model.Password,
+               model.RememberMe,
+               false);
+        }
+
+        public async Task LogoutAsync()
+        {
+            await _signInManager.SignOutAsync();
         }
     }
 }
