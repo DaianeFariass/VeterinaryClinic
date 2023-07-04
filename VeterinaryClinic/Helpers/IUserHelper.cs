e6d3e61e44904194065f6648b60a3d2f6c9efda3
Daiane Farias <daiane.farias.contractor@gmail.com>
2023-07-04 15:36:50 +0100

    Change user profile and password

--- VeterinaryClinic/Helpers/IUserHelper.cs
+++ VeterinaryClinic/Helpers/IUserHelper.cs
@@ -11,6 +11,8 @@ namespace VeterinaryClinic.Helpers
         Task<IdentityResult> AddUserAsync(User user, string password);
         Task<SignInResult> LoginAsync(LoginViewModel model);
         Task LogoutAsync();
+        Task<IdentityResult> UpdateUserAsync(User user);
+        Task<IdentityResult> ChangePasswordAsync(User user, string oldPassword, string newPassword);
 
     }
 }
