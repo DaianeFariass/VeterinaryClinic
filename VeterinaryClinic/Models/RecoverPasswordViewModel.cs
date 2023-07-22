e1280e21da26d6f2575ad4c38210103c8466b37e
Daiane Farias <daiane.farias.contractor@gmail.com>
2023-07-22 17:02:23 +0100

    Recover and reset password

---
+++ VeterinaryClinic/Models/RecoverPasswordViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VeterinaryClinic.Models
+{
+    public class RecoverPasswordViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+    }
+}
---
+++ VeterinaryClinic/Models/ResetPasswordViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace VeterinaryClinic.Models
+{
+    public class ResetPasswordViewModel
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password")]
+        [Display(Name = "Confirm Password")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        public string Token { get; set; }
+    }
+}
