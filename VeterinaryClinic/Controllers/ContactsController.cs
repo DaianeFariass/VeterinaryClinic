11f59d685af57cf06aedeac03e46af74b9220ee7
Daiane Farias <daiane.farias.contractor@gmail.com>
2023-07-27 18:16:38 +0100

    Change layout index Rooms Controller Contacts Controller

---
+++ VeterinaryClinic/Controllers/ContactsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VeterinaryClinic.Controllers
+{
+    public class ContactsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
